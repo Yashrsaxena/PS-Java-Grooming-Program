4a34361b03d24824cdd40f3616f3af61a522b522
Yashrsaxena <yashraj161999@gmail.com>
2023-04-19 01:34:32 +0530

    Week 2 - Serialization And Deserialization Problem - Made Necessary changes

--- src/SerializationAndDeserialization/Deserialization.java
+++ src/SerializationAndDeserialization/Deserialization.java
@@ -6,13 +6,12 @@ import java.io.ObjectInputStream;
 
 public class Deserialization {
 
+    //Deserializing method Load User that can be used to load user from the already existing file where the user data is saved
     static void loadUser(String filePath) throws IOException, ClassNotFoundException{
         User user = null;
-        try(FileInputStream fileInputStream = new FileInputStream(filePath);
-            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream)){
-            user = (User) objectInputStream.readObject();
-            objectInputStream.close();
-            fileInputStream.close();
+        try(FileInputStream fileInputStream = new FileInputStream(filePath); //Opening file
+            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream)){ // Giving the file to object input stream
+            user = (User) objectInputStream.readObject(); //reading object
         }
         finally {
             System.out.println("Deserializing User...");
@@ -24,6 +23,6 @@ public class Deserialization {
         }
     }
     public static void main(String[] args) throws IOException, ClassNotFoundException{
-        loadUser("SerializationAndDeserializationFile.txt");
+        loadUser("SerializationAndDeserializationFile.txt"); //loadUser method call
     }
 }
