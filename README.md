bf9b58b41d4f91f7432de7120783b16fbf5c4735
Yashrsaxena <yashraj161999@gmail.com>
2023-04-18 23:09:42 +0530

    Week 2 - Comparable and Comparator Problem Solved

--- README.md
+++
@@ -1,2 +0,0 @@
-# PS-Java-Grooming-Program
-Contains all the programs that solves the given problem statement in the Publicis Sapient March Grooming Program
---
+++ src/ComparableAndComparator/Main.java
@@ -0,0 +1,60 @@
+package ComparableAndComparator;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    //ComparableAndComparator.Book Class Implementation
+    //========================================================================================================
+    public static void main(String[] args) {
+        List<Book> books = new ArrayList<>();
+        //Adding values to the List
+        books.add(new Book("A", "F", 1999));
+        books.add(new Book("B", "H", 1996));
+        books.add(new Book("C", "G", 2011));
+        books.add(new Book("D", "J", 2001));
+        books.add(new Book("E", "I", 1986));
+
+        Collections.sort(books); //Sorting on the basis of Year Published in Increasing Order with the Comparable defined in the ComparableAndComparator.Book class
+
+        System.out.println("The ComparableAndComparator.Book data in a sorted fashion on the basis of Year Published");
+
+        for(Book b:books) // Printing out the result
+            System.out.println(b.getTitle()+" "+b.getAuthor()+" "+b.getYearPublished());
+        System.out.println();
+
+
+        //ComparableAndComparator.Employee Class Implementation
+        //========================================================================================================
+
+        List<Employee> employees = new ArrayList<>();
+        //Adding values to the List
+        employees.add(new Employee("A", 23, 25000));
+        employees.add(new Employee("B", 42, 26000));
+        employees.add(new Employee("C", 25, 62000));
+        employees.add(new Employee("D", 25, 65000));
+        employees.add(new Employee("E", 25, 35000));
+
+        employees.sort(new SortByAgeThenSalary());
+
+        System.out.println("The ComparableAndComparator.Employee data in a sorted fashion on the basis of Age and if age is same then Salary");
+
+        for(Employee e:employees) // Printing out the result
+            System.out.println(e.getName()+" "+e.getAge()+" "+e.getSalary());
+    }
+}
+
+class SortByAgeThenSalary implements Comparator<Employee>{
+    @Override
+    public int compare(Employee o1, Employee o2) {
+        if(o1.age-o2.age==0)
+        {
+            return o1.salary-o2.salary;
+        }
+        return o1.age-o2.age;
+    }
+}
