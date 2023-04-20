1fc30322f0ef8a93a064319f34779cc49e51b911
Yashrsaxena <yashraj161999@gmail.com>
2023-04-20 23:36:07 +0530

    Week 2 - File Handling initiated

---
+++ src/FileHandling/FileHandling.java
@@ -0,0 +1,7 @@
+package FileHandling;
+
+public class FileHandling {
+    public static void main(String[] args){
+        System.out.println("In this program I will be performing the task of File Handling and stuff according to the problem statement given in the assignment.");
+    }
+}
